03f38c648b960dc877f8710c30a24bbe4402223a
Danila Plater <danilaplyter@gmail.com>
2025-10-23 17:28:39 +0300

    init commit

---
+++ src/main/java/ru/mentee/power/TaskManagerSystemApplication.java
@@ -0,0 +1,13 @@
+/* @MENTEE_POWER (C)2025 */
+package ru.mentee.power;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TaskManagerSystemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TaskManagerSystemApplication.class, args);
+    }
+}
